8aecd12ac35a9e4689af9dea15ed0ae7f575115d
Jinjun Hu(Intern) <jjhu@bainainfo.com>
2015-04-29 14:03:52 +0800

    2015-4-29 14:03:33, let's go.

--- src/com/company/AddBinary.java
+++ src/com/company/AddBinary.java
@@ -1,20 +1,51 @@
 package com.company;
 
+
 /**
  * Created by jjhu on 2015/4/28.
  */
 public class AddBinary {
     public String addBinary(String a, String b) {
-        char []aChars = a.toCharArray();
-        char []bChars = b.toCharArray();
-        int len = Math.min(a.length(), b.length());
+        /*int mlen = Math.max(a.length(), b.length()), alen = a.length() - 1, blen = b.length() - 1;
         StringBuilder sb = new StringBuilder();
         int carry = 0;
-        for (int i = len - 1; i >= 0; i++){
-            int remainder = ((aChars[i] - '0') + (bChars[i] - '0') + carry) % 2;
-            carry = ((aChars[i] - '0') + (bChars[i] - '0') + carry) / 2;
-            sb.append(remainder);
+        for (int i = 0; i < mlen; i++){
+            int rem;
+            if (alen >= i && blen >= i){
+                rem = ((a.charAt(alen - i) - '0') + (b.charAt(blen - i) - '0') + carry) % 2;
+                carry = ((a.charAt(alen - i) - '0') + (b.charAt(blen - i) - '0') + carry) / 2;
+            }else if(alen >= i){
+                rem = ((a.charAt(alen - i) - '0') + carry) % 2;
+                carry = ((a.charAt(alen - i) - '0') + carry) / 2;
+            }else {
+                rem = ((b.charAt(blen - i) - '0') + carry) % 2;
+                carry = ((b.charAt(blen - i) - '0') + carry) / 2;
+            }
+            sb.insert(0, rem);
         }
-        return null;
+        if (carry == 1) {
+            sb.insert(0, 1);
+            return sb.toString();
+        }
+        return sb.toString();*/
+
+        int i = a.length()-1, j = b.length()-1, carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0 || carry == 1){
+            if (i >= 0){
+                carry += a.charAt(i--) - '0';
+            }
+            if (j >= 0){
+                carry += b.charAt(j--) - '0';
+            }
+            sb.insert(0, carry % 2);
+            carry /= 2;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        AddBinary ad = new AddBinary();
+        System.out.println(ad.addBinary("111", "11"));
     }
 }
